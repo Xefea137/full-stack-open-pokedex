9baf92fdd4315c449da0ce59bc6d754d8be5eb7f
Moe Khan <mobashir16@gmail.com>
2024-10-27 11:57:45 +0530

    11.19 Broken health check

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.get('/version', (req, res) => {
   res.send('8') // change this string to ensure a new version deployed
 })
 
-app.get('/heal', (req, res) => {
+app.get('/health', (req, res) => {
   res.send('ok')
 })
 
